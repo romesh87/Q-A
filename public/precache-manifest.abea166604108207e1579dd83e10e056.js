686201b7044d460a11ca6002b9b18dca748c8a34
romesh87 <roman.shishkin87@gmail.com>
2020-03-05 11:50:45 +0600

    prepared to deployment 2

---
+++ public/precache-manifest.abea166604108207e1579dd83e10e056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a0822c98c23617508c6fe1d73b557d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd21ec98e753831b554",
+    "url": "/static/css/main.f10d3e02.chunk.css"
+  },
+  {
+    "revision": "ae8c00140882a7b40e84",
+    "url": "/static/js/2.7564448b.chunk.js"
+  },
+  {
+    "revision": "3bf6c32fa60fa0f7b41615b984399950",
+    "url": "/static/js/2.7564448b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd21ec98e753831b554",
+    "url": "/static/js/main.83018de6.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
